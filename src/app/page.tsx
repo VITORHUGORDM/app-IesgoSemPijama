155756162f0be6c5a1ccf157959eb26cf7742617
VITORHUGORDM <vitrovitinho77@gmail.com>
2026-02-13 23:33:24 -0300

    iesgosempijama

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 text-black">
+      <h1>IESGOSEMPIJAMA</h1>
+    </div>
+  );
+}
